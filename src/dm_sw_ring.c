5e852610f23d08a885b94f523fd29de8ac031ddf
Patryk Kubiak <patryk.kubiak90@gmail.com>
2026-06-03 10:10:56 +0200

    Initialize DMOD Software Ring Module with CI configuration, CMake setup, and resource definitions

---
+++ src/dm_sw_ring.c
@@ -0,0 +1,30 @@
+#include "dmod.h"
+
+// ============================================================================
+//                      Module Interface Implementation
+// ============================================================================
+
+/**
+ * @brief Module initialization (optional)
+ */
+void dmod_preinit(void)
+{
+    // Nothing to do
+}
+
+/**
+ * @brief Module initialization
+ */
+int dmod_init(const Dmod_Config_t *Config)
+{
+    // Nothing to do
+    return 0;
+}
+
+/**
+ * @brief Module deinitialization
+ */
+void dmod_deinit(void)
+{
+    // Nothing to do
+}
